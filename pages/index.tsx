50b596bf242d41567ccc4b8a1640cb15a660ca1c
Omar Jaroudi <ojaroudi@turo.com>
2022-11-07 23:35:10 +0000

    env file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-   <div>ENV: {process.env.DEPLOY_ENV}-{process.env.NODE_ENV}</div>
+   <div>LOCAL: {process.env.LOCAL_DEPLOY_ENV} / ENV: {process.env.DEPLOY_ENV}-{process.env.NODE_ENV}</div>
   )
 }
