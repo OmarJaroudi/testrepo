a97ed3578406e954538a61ca47661256a280c832
Omar Jaroudi <ojaroudi@turo.com>
2022-11-16 15:27:47 -0800

    env

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 export default function Home() {
   return (
-   <div>{process.env.GITHUB_REF_NAME}</div>
+   <div>{process.env.GITHUB_REF_NAME} {process.env.DEPLOY_ENV}</div>
   )
 }
