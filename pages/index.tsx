d40980141ca9b6e1f485aa212845e84d634bf069
Omar Jaroudi <ojaroudi@turo.com>
2022-11-16 23:40:04 +0000

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
-export default function Home() {
-  return (
-   <div>{process.env.GITHUB_REF_NAME} {process.env.NODE_ENV}</div>
-  )
+export const getStaticProps = () => {
+  return {
+    props: {
+      env: process.env.GITHUB_REF_NAME
+    }
+  }
+}
+export default function Home({ env }: {env: string}) {
+  if (process.env.GITHUB_REF_NAME) {
+    return (
+      <div>{env}</div>
+     )
+  }
+
 }
